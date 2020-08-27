30052a3e2adba34a2cbe2efb5d7e99c840d63bac
Ben Stoller <stollersystemsound@gmail.com>
2020-08-26 22:11:51 -0700

    add create player UI
    Co-authored-by: Drew Lockhart <drewmlockhart@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 
+function Game() {
+  this.playerArray = []
+  this.playerId = 0
+}
+ let pigGame = new Game()
 
 
 
 
 
-
-
+function Player(Id,name,type) {
+  this.Id = Id
+  this.name = name
+  this.type = type
+  this.totalScore = 0
+  this.turnScore = 0
+}
 
 
 
@@ -26,11 +36,29 @@ $(document).ready(function(event) {
 function createPlayer(name,type) {
   console.log(name);
   console.log(type);
+  pigGame.playerId += 1;
+  let Id =  pigGame.playerId;
+  let player = new Player(Id,name,type)
+  pigGame.playerArray.push(player)
+  console.log(pigGame.playerArray)
+  displayPlayers(pigGame.playerArray)
 
   //let htmlToAdd = $("<p></p>").text("Text.");
 
-  let htmlToAdd = '<div class="player"><h2>'+name+'</h2><p>score</p></div>'
+
+function displayPlayers(array) {
+  console.log("display!")
+  let htmlToAdd = ""
+  pigGame.playerArray.forEach(function(player) {
+
+    htmlToAdd += '<div class="player" id="'+player.Id+'"><h2>'+player.name+' "'+player.type+'"'+'</h2><p>'+'Score:'+player.totalScore+'</p></div>'
+    console.log(htmlToAdd)
+  });
   $(".playerGroup").html(htmlToAdd);
+}
+
+
+  
 
 
 
